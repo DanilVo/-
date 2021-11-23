a4135c57769e362d50aeca21601f4030e6bfb521
DanilV <dani736@gmail.com>
2021-11-23 16:29:56 +0200

    finished task 3

---
+++ Вывести квадрат числа/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("enter your number:");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("result:");
+int result = A * A; 
+Console.WriteLine(result);
---
+++ По заданному номеру дня недели вывести его название/Program.cs
@@ -0,0 +1,35 @@
+﻿//string[] Days = {"sunday", "monday", "thursday", "wednesday", "thersday", "friday", "saturday"};
+Console.WriteLine("enter your day");
+string Days = Console.ReadLine();
+
+if(Days == "1")
+{
+    Console.WriteLine("sunday");
+}
+if(Days == "2")
+{
+    Console.WriteLine("monday");
+}
+if(Days == "3")
+{
+    Console.WriteLine("thursday");
+}
+if(Days == "4")
+{
+    Console.WriteLine("wednesday");
+}if(Days == "5")
+{
+    Console.WriteLine("thersday");
+}
+if(Days == "6")
+{
+    Console.WriteLine("friday");
+}
+if(Days == "7")
+{
+    Console.WriteLine("saturday");
+}
+else
+{
+    Console.WriteLine("there only 7 days!");
+}
