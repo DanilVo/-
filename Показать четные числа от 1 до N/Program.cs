96da76679e26e26eb13db214cf1b24a9a5facf4f
DanilV <Dani736@gmail.com>
2021-11-25 20:42:32 +0200

    fixed changes

---
+++ Показать четные числа от 1 до N/Program.cs
@@ -0,0 +1,18 @@
+﻿int num = 1;
+int index = 0;
+int max = 10;
+while(index<max)
+{
+    if(index<max)
+    {
+        Console.WriteLine(num);
+        if(index == 0)
+        {
+            num = num + 1;
+            Console.WriteLine(num);
+        }
+    }
+    num = num +2;
+    index++;
+}
+Console.WriteLine(num);
