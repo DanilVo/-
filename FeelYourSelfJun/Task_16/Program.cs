827bb98916c6720db27a9cd6e24628fd6a691657
DanilV <dani736@gmail.com>
2021-12-05 11:32:43 +0200

    finished task 18

---
+++ FeelYourSelfJun/Task_16/Program.cs
@@ -0,0 +1,18 @@
+﻿//Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.WriteLine("enter your day number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n>7 || n<1)
+{
+    Console.WriteLine("not such day");
+}
+else
+{
+    if(n==6 || n==7)
+    {
+        Console.WriteLine("weekend");
+    }
+    else
+    {
+        Console.WriteLine("week day");
+    }
+}
---
+++ FeelYourSelfJun/Task_18/Program.cs
@@ -0,0 +1,19 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+bool[] X = { true, false };
+bool[] Y = { true, false };
+for (int n = 0; n < X.Length; n++)
+{
+    for (int a = 0; a < Y.Length; a++)
+    {
+        if (!(X[n] || Y[a]) == (!X[n] && !Y[a]))
+        {
+            Console.WriteLine(true);
+        }
+        else
+        {
+            Console.WriteLine(false);
+        }
+    }
+
+}
