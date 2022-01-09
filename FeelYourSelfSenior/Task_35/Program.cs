63c5db98ec5c3d11b26281e85d4ba6a9b28d0455
DanilV <dani736@gmail.com>
2022-01-09 11:14:22 +0200

    commit to to commit with laptop

---
+++ FeelYourSelfSenior/Task_35/Program.cs
@@ -0,0 +1,18 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число
+
+int[] array = new int[12];
+int n = array.Length;
+Console.WriteLine("What num you want to check?");
+int a = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    array[i] = new Random().Next(0, 13);
+    if (array[i] == a)
+    {
+        Console.WriteLine("there is such number");
+    }
+    else
+    {
+        Console.WriteLine("no such number");
+    }
+}
