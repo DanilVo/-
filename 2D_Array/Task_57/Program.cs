6d794730d8aa89aae377b9358e329e6caf47f59a
DanilV <Dani736@gmail.com>
2022-01-14 23:58:07 +0200

    finished till task 56

---
+++ 2D_Array/Task_57/Program.cs
@@ -0,0 +1,29 @@
+﻿// Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива.
+
+int[,] fillArray(int[,] array)
+{
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+        for (int column = 0; column < array.GetLength(1); column++)
+        {
+            array[row,column] = new Random().Next(1,9);
+            Console.Write(array[row,column]);
+        }
+    Console.WriteLine();
+    }
+    return newArray;
+}
+
+int[,] arrangeArray(int[,] arr)
+{
+    for (int row = 0; row < arr.GetLength(0); row++)
+    {
+        for (int column = 0; column < arr.GetLength(1); column++)
+        {
+            
+        }
+    }
+}
+
+int[,] array = new int[6,6];
+int[,] arr = fillArray(array);
