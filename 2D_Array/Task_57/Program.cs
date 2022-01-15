87e5d25b89d631d5d65aab00614516793f53c909
DanilV <dani736@gmail.com>
2022-01-15 23:13:06 +0200

    work on task 57

--- 2D_Array/Task_57/Program.cs
+++ 2D_Array/Task_57/Program.cs
@@ -6,24 +6,37 @@ int[,] fillArray(int[,] array)
     {
         for (int column = 0; column < array.GetLength(1); column++)
         {
-            array[row,column] = new Random().Next(1,9);
-            Console.Write(array[row,column]);
+            array[row, column] = new Random().Next(1, 9);
+            Console.Write(array[row, column]);
         }
-    Console.WriteLine();
+        Console.WriteLine();
     }
-    return newArray;
+    return array;
 }
 
-int[,] arrangeArray(int[,] arr)
+int[,] arrangeArray(int[,] arr, int columnNumber)
 {
+    int max = 0;
     for (int row = 0; row < arr.GetLength(0); row++)
     {
         for (int column = 0; column < arr.GetLength(1); column++)
         {
-            
+            if (row == columnNumber)
+            {
+                if (arr[row,column] > max)
+                {
+                    max = arr[row,column];
+
+                }
+            Console.Write(max);
+            }
         }
+        Console.WriteLine();
     }
+    return arr;
 }
 
-int[,] array = new int[6,6];
+int[,] array = new int[4, 4];
 int[,] arr = fillArray(array);
+Console.WriteLine();
+arrangeArray(arr, 0);
